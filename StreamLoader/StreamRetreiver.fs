4d42c2900cda8d28e6c339a066a0caf74799bba1
Daniele Pozzobon <dnl.pozzobon@gmail.com>
2018-01-25 14:11:22 +0100

    Added Credentials to login

--- StreamLoader/StreamRetreiver.fs
+++ StreamLoader/StreamRetreiver.fs
@@ -1,24 +1,30 @@
 ﻿module StreamRetreiver
 open Logger
 open Polly
-open System.Configuration
-open System.IO
 open System.Net
 open System
+open FSharp.Data
+open Types
 
 let getPage baseUrl (logger:ILogger) (link:string)=
+    let config = Servers.Load "./config.json"
+
     let execute () =
         try
-            let geturl = (System.Net.WebRequest.Create (baseUrl + link))
-            
-            geturl.Timeout <- 300000
-            baseUrl, Some <| geturl.GetResponse().GetResponseStream()
-        with :? System.Net.WebException as ex->
+            let request = (WebRequest.Create (baseUrl + link))
+            let credentials = config.Credentials
+            if credentials.Use then
+                request.Credentials <- 
+                    NetworkCredential(credentials.User, credentials.Password, credentials.Domain)
+
+            request.Timeout <- 300000
+            baseUrl, Some <| request.GetResponse().GetResponseStream()
+        with :? WebException as ex->
             logger.error <| sprintf "%s - %s" link ex.Message
             baseUrl, None
 
     Policy
-        .Handle<System.Net.WebException>()
+        .Handle<WebException>()
         .Retry(3,fun x i -> 
             logger.info  <| sprintf "exception opening %s%s" baseUrl link
             logger.error ("exeption opening",x))
